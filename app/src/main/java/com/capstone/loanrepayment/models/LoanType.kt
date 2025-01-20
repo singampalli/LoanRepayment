61589226a70451c91227b500f1c14eaaa91e8ce4
sandeep kumar <sandeepkumar.singampalli@gmail.com>
2025-01-20 18:54:37 +0530

    loans list worked

--- app/src/main/java/com/capstone/loanrepayment/models/LoanType.kt
+++ app/src/main/java/com/capstone/loanrepayment/models/LoanType.kt
@@ -1,6 +1,18 @@
 package com.capstone.loanrepayment.models
 
 data class LoanType(
-    val id:Int,
-    val loanType:String
+    val data: List<Data>,
+    val total: Int
+)
+
+data class Data(
+    val id: Int,
+    val loanAccountNumber: String,
+    val loanType: String,
+    val loanAmount: String,
+    val loanEMI: String,
+    val loanStartDate: String,
+    val loanEndDate: String,
+    val loanStatus: String,
+    val username: String
 )
