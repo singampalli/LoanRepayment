7beeee390ce6f1c6937d3e23a42e8d22eb95b06b
sandeep kumar <sandeepkumar.singampalli@gmail.com>
2025-01-27 21:11:44 +0530

    message handling

---
+++ app/src/main/java/com/capstone/loanrepayment/BaseActivity.kt
@@ -0,0 +1,20 @@
+package com.capstone.loanrepayment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_base)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
