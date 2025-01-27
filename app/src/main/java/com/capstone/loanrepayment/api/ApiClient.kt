7d46cb34d02768587671638f6270d61eadd94915
sandeep kumar <sandeepkumar.singampalli@gmail.com>
2025-01-28 04:19:44 +0530

    signup final

--- app/src/main/java/com/capstone/loanrepayment/api/ApiClient.kt
+++ app/src/main/java/com/capstone/loanrepayment/api/ApiClient.kt
@@ -3,7 +3,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+//    private const val BASE_URL = "http://10.0.2.2:3000/"
+    private const val BASE_URL = "http://192.168.0.107:3000/"
 
     val instance: Retrofit by lazy {
         Retrofit.Builder()
